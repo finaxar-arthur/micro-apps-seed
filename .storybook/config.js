c34aae7e3177c0f040e090a8f6487fd139c99fb1
Arthur Vincent Simon <arthur@finaxar.com>
2019-04-15 11:47:53 +0800

    Colocate stories to the components

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,18 +1,17 @@
-import { configure, addParameters } from '@storybook/react';
-import '../src/App.css';
+import { configure, addParameters } from "@storybook/react";
+import "../src/App.css";
 
 addParameters({
   options: {
-    brandTitle: 'Micro Apps Seed'
-  },
+    brandTitle: "Micro Apps Seed"
+  }
 });
 
 function loadStories() {
-  require('../stories/index.js');
-
   // automatically import all story js files that end with *.stories.tsx
   // const req = require.context('../stories', true, /\.stories\.tsx$/);
-  // req.keys().forEach(filename => req(filename));
+  const req = require.context("../src", true, /\.story\.js$/);
+  req.keys().forEach(filename => req(filename));
 }
 
 configure(loadStories, module);
--- src/components/Button.story.js
+++ src/components/Button.story.js
@@ -1,10 +1,8 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 
-import Loading from "../src/components/Loading";
-import Button from "../src/components/Button";
+import Button from "./Button";
 
-storiesOf("Loading", module).add("default", () => <Loading />);
 storiesOf("Button", module)
   .add("default", () => <Button>Default</Button>)
   .add("primary", () => <Button type="primary">Primary</Button>);
